214e86f6585fdff609be2372a9d9033381482054
Mario <mariob360@gmail.com>
2018-03-14 23:38:00 -0700

    add toDo schema in todo.js

---
+++ todos_api/models/todo.js
@@ -0,0 +1,24 @@
+// schema file
+
+var mongoose = require("mongoose");
+
+var todoSchema = new mongoose.Schema({
+  name: {
+    type: String, 
+    required: "Please enter a name"
+  },
+  complete: {
+    type: Boolean, 
+    default: false
+  },
+  created_date: {
+    type: Date,
+    default: Date.now // will be autofilled with the time of completion 
+  }
+
+})
+
+var Todo = mongoose.model("Todo", todoSchema) // (name of schema, Schema)
+
+module.exports = Todo; // ready to exports the above model ^^
+
