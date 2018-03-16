6a950a9c7880d19f096cd14fa3b0c157fda4839a
Mario <mariob360@gmail.com>
2018-03-15 22:25:42 -0700

    add findoneandupdate route for todos

--- todos_api/routes/todos.js
+++ todos_api/routes/todos.js
@@ -24,18 +24,28 @@ router.post("/", function(req, res) {
   .catch(function(err) {
     res.send(err);
   })
-})
+});
 
 // Retrieve single todo
 router.get("/:todoId", function(req, res) {
-  db.Todo.findById(req.params.todoId)
+  db.Todo.findById(req.params.todoId) //url params used to find the specific todo
   .then(function(foundTodo) {
     res.json(foundTodo);
   })
   .catch(function(err) {
     res.send(err);
   })
-})
+});
 
+// Retrieve single todo and update it
+router.put("/:todoId", function(req, res) {
+  db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true})
+  .then(function(todo) {
+    res.json(todo);
+  })
+  .catch(function(err) {
+    res.send(err);
+  })
+})
 
 module.exports = router; //give access to these routes in index.js
