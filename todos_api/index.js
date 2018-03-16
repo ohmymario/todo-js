d02ebd3deb102d685d58ae32704e09616559785f
Mario <mariob360@gmail.com>
2018-03-15 20:19:54 -0700

    require body-parser in index.js

--- todos_api/index.js
+++ todos_api/index.js
@@ -1,9 +1,13 @@
 var express = require("express"), 
-    port = process.env.PORT || 3000;
+    port = process.env.PORT || 3000,
+    bodyParser = require("body-parser");
 
 var app = express();
-
 var todoRoutes = require("./routes/todos");
+
+// allow to access the body of a post/put request
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/", function(req,res) {
   res.send("Hello from the root route"); // passing in an object reflects on the browser as json
