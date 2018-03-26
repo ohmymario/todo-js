473d11c4b26a59e9cb62fbf7df04621a4c65b999
Mario <mariob360@gmail.com>
2018-03-26 15:58:31 -0700

    removed db requirement

--- todos_api/routes/todos.js
+++ todos_api/routes/todos.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var router = express.Router();
-var db = require("../models") //only need models directory since all models are required in the index.js file
 var helpers = require("../helpers/todos");
 
 // actual route = /api/todos/
