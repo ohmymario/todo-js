7624f9b6300ae05bd6e58febcd52f0eca9202d0e
Mario <mariob360@gmail.com>
2018-03-26 18:35:47 -0700

    add class for when todo is completed

--- todos_api/public/app.js
+++ todos_api/public/app.js
@@ -1,16 +1,19 @@
-$(document).ready(function() {
+$(document).ready(function () {
   "/api/todos"
 
   $.getJSON("/api/todos")
-  .done(addTodos)
-  .fail(function() {
-    console.log("Something went wrong !");
-  })
+    .done(addTodos)
+    .fail(function () {
+      console.log("Something went wrong !");
+    })
 })
 
-function addTodos(todos){
-  todos.forEach(function(todo) {
-    var newTodo = $('<li class="task">'+todo.name+'</li>');
+function addTodos(todos) {
+  todos.forEach(function (todo) {
+    var newTodo = $('<li class="task">' + todo.name + todo.complete + '</li>');
+    if (todo.complete) {
+      newTodo.addClass('done');
+    }
     $(".list").append(newTodo);
-  }) 
+  })
 }
