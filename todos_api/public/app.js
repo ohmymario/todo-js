14c09958b998bbfbc577aeaecdb590626839afdb
Mario <mariob360@gmail.com>
2018-03-26 20:12:48 -0700

    add remove ability

--- todos_api/public/app.js
+++ todos_api/public/app.js
@@ -9,6 +9,11 @@ $(document).ready(function () {
       if(event.which == 13) {
         createTodo();
       }
+    });
+
+    // span is generated after page load so listener on list class
+    $(".list").on('click', 'span', function() { 
+      removeTodo($(this).parent());
     })
 })
 
@@ -19,7 +24,8 @@ function addTodos(todos) {
 }
 
 function addTodo(todo) {
-  var newTodo = $('<li class="task">' + todo.name+ '</li>');
+  var newTodo = $('<li class="task">' + todo.name +'<span>X</span></li>');
+  newTodo.data('id', todo._id);
   if (todo.complete) {
     newTodo.addClass('done');
   }
@@ -30,9 +36,22 @@ function createTodo() {
   var userInput = $("#todoInput").val();
   $.post("/api/todos", {name: userInput})
   .then(function(newTodo) {
+    $("#todoInput").val("");
     addTodo(newTodo);
   })
   .catch(function(err) {
     console.log(err);
+  })
+}
+
+function removeTodo(todo) {
+  var clickedId = todo.data('id');
+  var deleteUrl = '/api/todos/' + clickedId;
+  $.ajax({
+    method: 'DELETE',
+    url: deleteUrl
+  })
+  .then(function (data) {
+    todo.remove();
   })
 }
