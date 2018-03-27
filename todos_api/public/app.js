3b0c5db3aff4e651a38f2ba26fa6bf5108901d85
Mario <mariob360@gmail.com>
2018-03-26 18:24:24 -0700

    add AJAX call with jQuery to our API

--- todos_api/public/app.js
+++ todos_api/public/app.js
@@ -1,1 +1,16 @@
-alert("hi from app.js")
+$(document).ready(function() {
+  "/api/todos"
+
+  $.getJSON("/api/todos")
+  .done(addTodos)
+  .fail(function() {
+    console.log("Something went wrong !");
+  })
+})
+
+function addTodos(todos){
+  todos.forEach(function(todo) {
+    var newTodo = $('<li class="task">'+todo.name+'</li>');
+    $(".list").append(newTodo);
+  }) 
+}
